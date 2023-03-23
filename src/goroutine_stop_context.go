7c69238a98188a55029003d8872fd93b45bad127
icoding168 <icoding168@qq.com>
2023-03-23 10:28:49 +0800

    让协程退出，用context支持多个协程

--- src/goroutine_stop_context.go
+++ src/goroutine_stop_context.go
@@ -9,18 +9,26 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
-	wg.Add(1)
-	ctx, stop := context.WithCancel(context.Background())
-	go func() {
-		defer wg.Done()
-		watchDog(ctx, "【监控狗1】")
-	}()
-	time.Sleep(5 * time.Second) //先让监控狗监控5秒
-	stop()                      //发停止指令
+	ctx, cancel := context.WithCancel(context.Background())
+
+	for i := 0; i < 5; i++ { // 启动 5 个监控狗协程
+		wg.Add(1)
+		go func(name string) {
+			defer wg.Done()
+			watchDog(ctx, name)
+		}(fmt.Sprintf("【监控狗%d】", i+1))
+	}
+
+	time.Sleep(5 * time.Second) // 先让监控狗监控 5 秒
+
+	// 发送停止指令到所有监控狗的 context 中
+	cancel()
+
 	wg.Wait()
 }
+
 func watchDog(ctx context.Context, name string) {
-	//开启for select循环，一直后台监控
+	// 开启 for select 循环，一直后台监控
 	for {
 		select {
 		case <-ctx.Done():
