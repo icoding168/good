c525eb73cadb9572a02c9f71d0ff52e41ffbed2f
icoding168 <icoding168@qq.com>
2023-03-24 15:09:01 +0800

    增加web框架

--- src/framework/orm/geeorm_test.go
+++ src/framework/orm/geeorm_test.go
@@ -11,7 +11,7 @@ import (
 
 func OpenDB(t *testing.T) *Engine {
 	t.Helper()
-	engine, err := NewEngine("sqlite3", "gee.db")
+	engine, err := NewEngine("sqlite3", "../gee.db")
 	if err != nil {
 		t.Fatal("failed to connect", err)
 	}
